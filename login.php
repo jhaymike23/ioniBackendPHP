6e023b8f7c0d328eadbd8ce57846ee59e394c110
Hayme The Great <jameskyurem@gmail.com>
2019-08-30 20:16:21 +0800

    modifications for backend

--- login.php
+++ login.php
@@ -1,36 +1,40 @@
 <?php  
-    header("Access-Control-Allow-Origin: *");
-	  header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Methods: GET");
+  header("Access-Control-Allow-Origin: *");
+  header("Content-Type: application/json; charset=UTF-8");
+  header("Access-Control-Allow-Methods: GET");
 
-    include('config.ini.php');
-
-
-   $contentdata  = file_get_contents("php://input");
-   $getdata = json_decode($contentdata);
-
-   $user =   $getdata->user; 
-   $pass = $getdata->pass;
-   $error = "Unknown account";
-   /*$user =   $_POST['user']; 
-   $pass = $_POST['pass'];*/
+  include('config.ini.php');
 
 
-   $sql = "SELECT * FROM member WHERE mem_use = '$user' AND mem_pass = '$pass' ";
-   $result = mysqli_query($con,$sql);
+  $contentdata  = file_get_contents("php://input");
+  $getdata = json_decode($contentdata);
 
-   $numrow = mysqli_num_rows($result);
+  $user =   $getdata->user; 
+  $pass = $getdata->pass;
+
+  // should crete something like this
+  $data = new stdClass();
+  $data->status = 0;
+  $data->data = null;
+
+
+  $sql = "SELECT * FROM member WHERE mem_use = '$user' AND mem_pass = '$pass' ";
+  $result = mysqli_query($con,$sql);
+
+  $numrow = mysqli_num_rows($result);
   
   if($numrow == 1){
-       $arr = array();
-       while($row = mysqli_fetch_assoc($result)){
-         $arr[] = $row;
-       }
+
+        $arr = array();
+
+        while($row = mysqli_fetch_assoc($result)){
+          $arr[] = $row;
+        }
    
-      echo json_encode($arr);
-  }else{
-      echo json_encode($error);
+      $data->status = 1;
+      $data->data = $arr;
   }
-  
+
+  echo json_encode($data);
    
 ?>
